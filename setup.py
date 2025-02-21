d095eb994e70d5be1d669edb05e09fee5554cba0
AdrienEY <adrien.payen@be.ey.com>
2025-02-21 10:11:20 +0100

    modif setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+# Fonction pour récupérer tous les fichiers de configuration au niveau de la racine
+def find_config_files():
+    config_files = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith((".yml", ".yaml", ".json", ".ini", ".conf", ".cfg")):  # Ajoutez ici les extensions des fichiers config
+                config_files.append(os.path.relpath(os.path.join(root, file)))
+    return config_files
 
 setup(
     name='FrugalGPT',
@@ -6,8 +16,9 @@ setup(
     author='Lingjiao Chen, Matei Zaharia, and James Zou',
     author_email='lingjiao@stanford.edu',
     description='The FrugalGPT library',
-    packages=find_packages(where="src"),  # Search for packages under src
-    package_dir={"": "src"},  # Declares that the source code is in the 'src' directory
+    # Recherche des packages dans 'src' uniquement
+    packages=find_packages(where="src"),  
+    package_dir={"": "src"},  # Déclare que le code source est dans 'src'
     install_requires=[
         'numpy',
         'cohere',
@@ -24,6 +35,10 @@ setup(
         'accelerate',
         'ai21',
     ],
+    # Inclure les fichiers de config dans le package data
+    package_data={
+        '': find_config_files(),  # Inclut les fichiers de configuration trouvés
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
